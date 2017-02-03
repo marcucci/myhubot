1052410dd081cd62a11a07a15059069cec82d5d9
marcucci <dmarcucci@gmail.com>
2017-02-03 07:13:11 -0500

    added hubot-auth

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -3,5 +3,6 @@
 call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
+SET HUBOT_AUTH_ADMIN="shell,marcucci"
 
-node_modules\.bin\hubot.cmd --name "gitbot" %* 
+node_modules\.bin\hubot.cmd --name "gitbot" %*
