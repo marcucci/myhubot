dacd5a0a8ad15b1171f4cb1338361b70b8fde254
David Marcucci <david.marcucci@hpe.com>
2017-02-14 14:39:26 -0500

    hubot.cmd update
    added example env variables to cmd file

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -3,6 +3,15 @@
 call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
-SET HUBOT_AUTH_ADMIN="shell,marcucci"
+
+SET HUBOT_AUTH_ADMIN="shell"
+
+SET AGM_clientId=api_client_example_1
+SET AGM_clientSecret=05example45g
+SET AGM_apiUrl=https://agilemanager-itg.saas.hp.com/
+
+SET github_api_url=https://api.github.com/v3
+SET github_owner=marcucci
+SET github_repo=test
 
 node_modules\.bin\hubot.cmd --name "gitbot" %*
