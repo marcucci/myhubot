717582cca7a1c0adef2a57d06fdec2fe7944e51c
Degennaro <anthony.degennaro@hpe.com>
2017-02-27 14:05:02 -0500

    converted functions into single promise chain

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -30,54 +30,105 @@ module.exports = (robot) => {
     // get page count for github issues api call, create a promise
     // for each page, iterate over resulting arrays and build issue
     // object as well as issue id arrays
-    function findAllIssues() {
-      let getPageCount = new Promise((resolve, reject) => {
-        github.get(`${issuesUrl}&page=1`, (issues) => {
-          let pageCount = Math.ceil(issues[0].number / 100);
+    // function findAllIssues() {
+    //   let issuesPromises = [];
+    //   let getPageCount = new Promise((resolve, reject) => {
+    //     github.get(`${issuesUrl}&page=1`, (issues) => {
+    //       let pageCount = Math.ceil(issues[0].number / 100);
+    //       // need if else for success/fail
+    //       resolve(pageCount);
+    //     });
+    //   });
+    //
+    //   function buildIssuesPromises(num) {
+    //     while (num > 0) {
+    //       issuesPromises.push(get100Issues(num));
+    //       num -= 1;
+    //     }
+    //     return issuesPromises;
+    //   }
+    //
+    //   function get100Issues(num) {
+    //     return new Promise((resolve, reject) => {
+    //       github.get(`${issuesUrl}&page=${num}`, (issues) => {
+    //         // need if else for success/fail
+    //         resolve(issues);
+    //       });
+    //     });
+    //   }
+    //
+    //   function buildIssueObjects(arr) {
+    //     arr.map(issues => {
+    //       issues.map(issue => {
+    //         buildIssueObject(issue);
+    //         allIssueIds.push(issue.number);
+    //       })
+    //     })
+    //     allIssues.objects = allIssueObjects;
+    //     allIssues.ids = allIssueIds;
+    //     res.reply(allIssueIds);
+    //   }
+    //
+    //   getPageCount.then(num => {
+    //     return buildIssuesPromises(num);
+    //   }).then((promises) => {
+    //     return Promise.all(promises);
+    //   }).then(data => {
+    //     buildIssueObjects(data);
+    //   }).catch(err => {
+    //     res.reply(err);
+    //   })
+    // }
+
+    // fetch(`${issuesUrl}&page=1`)
+    //   .then(response => {
+    //     console.log(response.status);
+    //     return response.json();
+    //   })
+    //   .then(issues => {
+    //     let pageCount = Math.ceil(issues[0].number / 100);
+    //     console.log(pageCount);
+    //   });
+
+    let issuesPromises = [];
+    let commentsPromises = [];
+
+    let issuesPageCount = new Promise((resolve, reject) => {
+      github.get(`${issuesUrl}&page=1`, (issues) => {
+        let pageCount = Math.ceil(issues[0].number / 100);
+        // need if else for success/fail
+        resolve(pageCount);
+      });
+    });
+
+    function buildIssuesPromises(num) {
+      while (num > 0) {
+        issuesPromises.push(get100Issues(num));
+        num -= 1;
+      }
+      return issuesPromises;
+    }
+
+    function get100Issues(num) {
+      return new Promise((resolve, reject) => {
+        github.get(`${issuesUrl}&page=${num}`, (issues) => {
           // need if else for success/fail
-          resolve(pageCount);
+          resolve(issues);
         });
       });
+    }
 
-      function formatIssues(num) {
-        let promises = [];
-        while (num > 0) {
-          promises.push(get100Issues(num));
-          num -= 1;
-        }
-
-        Promise.all(promises).then(data => {
-          buildIssueObjects(data);
-        }).catch(err => {
-          res.reply(err);
-        })
-      }
-
-      function get100Issues(num) {
-        return new Promise((resolve, reject) => {
-          github.get(`${issuesUrl}&page=${num}`, (issues) => {
-            // need if else for success/fail
-            resolve(issues);
-          });
-        });
-      }
-
-      function buildIssueObjects(arr) {
-        arr.map(issues => {
-          issues.map(issue => {
+    function buildIssueObjects(arr) {
+      arr.map(issues => {
+        issues.map(issue => {
+          if (!issue.hasOwnProperty('pull_request')) {
             buildIssueObject(issue);
             allIssueIds.push(issue.number);
-          })
+          }
         })
-        allIssues.objects = allIssueObjects;
-        allIssues.ids = allIssueIds;
-      }
-
-      getPageCount.then(num => {
-        formatIssues(num);
-      }).catch(err => {
-        res.reply(err);
       })
+      allIssues.objects = allIssueObjects;
+      allIssues.ids = allIssueIds;
     }
 
     function buildIssueObject(array) {
@@ -119,63 +170,65 @@ module.exports = (robot) => {
       return null;
     }
 
-    function findLinkedIssues() {
-      let getPageCount = fetch(`${commentsUrl}&page=1`).then(res => {
-        let url = res.headers.get('Link').split(" ")[2];
-        return url.split("&page=")[1].charAt(0);
-      }).catch(err => {
-        res.reply(err);
-      });
+    let commentsPageCount = fetch(`${commentsUrl}&page=1`).then(res => {
+      let url = res.headers.get('Link').split(" ")[2];
+      return url.split("&page=")[1].charAt(0);
+    }).catch(err => {
+      res.reply(err);
+    });
 
-      function scanAllComments(num) {
-        let promises = [];
-        while (num > 0) {
-          promises.push(scan100Comments(num));
-          num -= 1;
-        }
-        Promise.all(promises).then(data => {
-          console.log(linkedIssues);
-        }).catch(err => {
-          res.reply(err);
-        })
+    function buildCommentsPromises(num) {
+      while (num > 0) {
+        commentsPromises.push(scan100Comments(num));
+        num -= 1;
       }
-
-      function scan100Comments(num) {
-        return new Promise((resolve, reject) => {
-          github.get(`${commentsUrl}&page=${num}`, (comments) => {
-            for(comment of comments) {
-              if (comment.body.includes("Linked to Agile Manager ID #")) {
-                let id = comment.issue_url.split('/issues/');
-                linkedIssues.push(parseInt(id[1]));
-              }
-            }
-            // need if else for success/fail
-            resolve(linkedIssues);
-          });
-        });
-      }
-
-      getPageCount.then(data => {
-        scanAllComments(data);
-      }).catch(err => {
-        res.reply(err);
-      })
+      return commentsPromises;
     }
 
-    function createAgmItems(unlinkedIssues){
-      // unlinkedIssues.map((num) => {
-      //   let match = matchIssueObject(num, allIssueObjects);
-      //   createItemThenComment(match);
+    function scan100Comments(num) {
+      return new Promise((resolve, reject) => {
+        github.get(`${commentsUrl}&page=${num}`, (comments) => {
+          for(comment of comments) {
+            if (comment.body.includes("Linked to Agile Manager ID #")) {
+              let id = comment.issue_url.split('/issues/');
+              linkedIssues.push(parseInt(id[1]));
+            }
+          }
+          // need if else for success/fail
+          resolve(linkedIssues);
+        });
+      });
+    }
+
+    // function createAgmItems(unlinkedIssues){
+      // unlinkedIssues.map(num => {
+      //   if (num < 2) {
+      //     let match = matchIssueObject(num, allIssueObjects);
+      //     return createItemThenComment(match);
+      //   }
       // });
       //  let match = matchIssueObject(32, allIssueObjects);
       //  testPromiseChain(match);
+    // }
+
+    let agmPromises = [];
+
+    function buildAgmPromises(unlinkedIssues) {
+      unlinkedIssues.map(num => {
+        if (num < 3) {
+          let match = matchIssueObject(num, allIssueObjects);
+          agmPromises.push(createAgmItem(match));
+        }
+      });
+      return agmPromises;
     }
 
-    function testPromiseChain(match) {
-      let createAgmItem = new Promise((resolve, reject) => {
+    function createAgmItem(match) {
+      return new Promise((resolve, reject) => {
         let resourceOptions = createResourceOptions(match);
         agm.resource(resourceOptions, function(err, body) {
           if (err) {
+            console.log(err);
             reject(err);
           } else {
             // agmDetails - Item id, API id, Url, Terminal Message
@@ -190,29 +243,52 @@ module.exports = (robot) => {
           };
         });
       });
-
-      function postGithubComment(data) {
-        console.log(data[3]);
-        let comment = {"body": `Linked to Agile Manager ID #${data[0]} (API ID #${data[1]})`}
-        let commentSuccess = "Github Comment Created"
-
-        let postComment = new Promise((resolve, reject) => {
-          github.post (`${data[2]}/comments`, comment, function(err, reply) {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(commentSuccess);
-            }
-          });
-        });
-      }
-
-      createAgmItem.then(data => {
-        postGithubComment(data);
-      }).catch(err => {
-        res.reply(err);
-      })
     }
+
+    // function createItemThenComment(match) {
+    //   let createAgmItem = new Promise((resolve, reject) => {
+    //     let resourceOptions = createResourceOptions(match);
+    //     agm.resource(resourceOptions, function(err, body) {
+    //       if (err) {
+    //         console.log(err);
+    //         reject(err);
+    //       } else {
+    //         // agmDetails - Item id, API id, Url, Terminal Message
+    //         let agmDetails = [
+    //           body.data[0].item_id,
+    //           body.data[0].id,
+    //           match.url,
+    //           `Item Created. AGM Client ID: ${body.data[0].item_id}. Github Issue: ${match.number}`
+    //         ]
+    //
+    //         resolve(agmDetails);
+    //       };
+    //     });
+    //   });
+    //
+    //   function postGithubComment(data) {
+    //     console.log(data[3]);
+    //     let comment = {"body": `Linked to Agile Manager ID #${data[0]} (API ID #${data[1]})`}
+    //     let commentSuccess = "Github Comment Created"
+    //
+    //     let postComment = new Promise((resolve, reject) => {
+    //       github.post (`${data[2]}/comments`, comment, function(err, reply) {
+    //         if (err) {
+    //           reject(err);
+    //         } else {
+    //           resolve(commentSuccess);
+    //         }
+    //       });
+    //     });
+    //   }
+    //
+    //   createAgmItem.then(data => {
+    //     console.log(data);
+    //     // postGithubComment(data);
+    //   }).catch(err => {
+    //     res.reply(err);
+    //   })
+    // }
 
     function matchIssueObject(num, issues){
       let match = issues.filter(function(obj) {
@@ -247,10 +323,29 @@ module.exports = (robot) => {
       }
     }
 
-    // findAllIssues();
-    // findLinkedIssues();
-    // let unlinkedIssues = _.difference(allIssueIds, linkedIssues);
-    // console.log(unlinkedIssues);
+    issuesPageCount.then(num => {
+      return buildIssuesPromises(num);
+    }).then(promises => {
+      return Promise.all(promises);
+    }).then(data => {
+      buildIssueObjects(data);
+    }).then(() => {
+      return commentsPageCount;
+    }).then(num => {
+      return buildCommentsPromises(num);
+    }).then(promises => {
+      return Promise.all(promises);
+    }).then(() => {
+      return _.difference(allIssueIds, linkedIssues);
+    }).then(unlinkedIssues => {
+      return buildAgmPromises(unlinkedIssues);
+    }).then(promises => {
+      return Promise.all(promises);
+    }).then(data => {
+      console.log(data);
+    }).catch(err => {
+      res.reply(err);
+    })
 
     // let p1 = Promise.all([findAllIssues, findLinkedIssues]);
     //
