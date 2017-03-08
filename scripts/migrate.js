9778a0c1c7b65a0d0fc7c94f4d5bc6659ac69033
Degennaro <anthony.degennaro@hpe.com>
2017-03-08 16:47:16 -0500

    added find feature id function to update issue command

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -290,6 +290,108 @@ module.exports = (robot) => {
     let issueUrl    = `https://github.hpe.com/api/v3/repos/Centers-of-Excellence/EA-Marketplace-Design-Artifacts/issues/${num}`;
     let issueObject = {};
     let apiId;
+    let featureIds = [
+      {
+        "gh_label"  : "Feature: Single Sign On",
+        "No Sprint" : "6763",
+        "Sprint 1"  : "7064",
+        "Sprint 2"  : "7064",
+        "Sprint 3"  : "7074",
+        "Sprint 4"  : "7074",
+        "Sprint 5"  : "7084",
+        "Sprint 6"  : "7084"
+      },
+      {
+        "gh_label"  : "Feature: Marketplace Compliance Toolkit Access",
+        "No Sprint" : "6769",
+        "Sprint 1"  : "7065",
+        "Sprint 2"  : "7065",
+        "Sprint 3"  : "7075",
+        "Sprint 4"  : "7075",
+        "Sprint 5"  : "7085",
+        "Sprint 6"  : "7085"
+      },
+      {
+        "gh_label"  : "Feature: Basic RBAC Support",
+        "No Sprint" : "6770",
+        "Sprint 1"  : "7066",
+        "Sprint 2"  : "7066",
+        "Sprint 3"  : "7076",
+        "Sprint 4"  : "7076",
+        "Sprint 5"  : "7086",
+        "Sprint 6"  : "7086"
+      },
+      {
+        "gh_label"  : "Feature: Personalization",
+        "No Sprint" : "6771",
+        "Sprint 1"  : "7067",
+        "Sprint 2"  : "7067",
+        "Sprint 3"  : "7077",
+        "Sprint 4"  : "7077",
+        "Sprint 5"  : "7087",
+        "Sprint 6"  : "7087"
+      },
+      {
+        "gh_label"  : "Feature: QlikView/QlikSense Report Integration",
+        "No Sprint" : "6773",
+        "Sprint 1"  : "7068",
+        "Sprint 2"  : "7068",
+        "Sprint 3"  : "7078",
+        "Sprint 4"  : "7078",
+        "Sprint 5"  : "7088",
+        "Sprint 6"  : "7088"
+      },
+      {
+        "gh_label"  : "Feature: User data upload to Vertica",
+        "No Sprint" : "6774",
+        "Sprint 1"  : "7069",
+        "Sprint 2"  : "7069",
+        "Sprint 3"  : "7079",
+        "Sprint 4"  : "7079",
+        "Sprint 5"  : "7089",
+        "Sprint 6"  : "7089"
+      },
+      {
+        "gh_label"  : "Feature: User data upload to Hadoop",
+        "No Sprint" : "6775",
+        "Sprint 1"  : "7070",
+        "Sprint 2"  : "7070",
+        "Sprint 3"  : "7080",
+        "Sprint 4"  : "7080",
+        "Sprint 5"  : "7090",
+        "Sprint 6"  : "7090"
+      },
+      {
+        "gh_label"  : "Feature: User data download- Data Extract Capability",
+        "No Sprint" : "6776",
+        "Sprint 1"  : "7071",
+        "Sprint 2"  : "7071",
+        "Sprint 3"  : "7081",
+        "Sprint 4"  : "7081",
+        "Sprint 5"  : "7091",
+        "Sprint 6"  : "7091"
+      },
+      {
+        "gh_label"  : "Feature: Receive Notifications for data upload/extract completion",
+        "No Sprint" : "6778",
+        "Sprint 1"  : "7072",
+        "Sprint 2"  : "7072",
+        "Sprint 3"  : "7082",
+        "Sprint 4"  : "7082",
+        "Sprint 5"  : "7092",
+        "Sprint 6"  : "7092"
+      },
+      {
+        "gh_label"  : "Feature: Chat with an agent",
+        "No Sprint" : "6777",
+        "Sprint 1"  : "7073",
+        "Sprint 2"  : "7073",
+        "Sprint 3"  : "7083",
+        "Sprint 4"  : "7083",
+        "Sprint 5"  : "7093",
+        "Sprint 6"  : "7093"
+      }
+    ];
 
     let getIssueComments = new Promise(function(resolve, reject) {
       GITHUB.get(`${issueUrl}/comments`, comments => {
@@ -321,6 +423,27 @@ module.exports = (robot) => {
       issueObject.storyPoints = findStoryPoints(i.labels);
       issueObject.state = convertState(i.state);
       issueObject.priority = findPriority(i.labels);
+      issueObject.featureId = findFeatureId(i);
+    }
+
+    function findFeatureId(i) {
+      let feature = _.find(i.labels, l => { return l.name.includes('Feature') });
+
+      if (feature === undefined) {
+        return null;
+      }
+
+      //find featureIds object via issue's feature name
+      let obj = _.find(featureIds, f => { return f.gh_label === feature.name })
+
+      //if no assigned sprint, use 'no sprint' feature id
+      if (i.milestone === null) {
+        return obj['No Sprint'];
+      }
+
+      //use sprint # to find feature id
+      let sprint = i.milestone.title.split('- ')[1];
+      return obj[sprint];
     }
 
     function findStoryPoints(labels) {
@@ -385,7 +508,8 @@ module.exports = (robot) => {
               name: obj.title,
               story_points: obj.storyPoints,
               story_priority: obj.priority,
-              status: obj.state //New, In Progress, In Testing, or Done
+              status: obj.state, //New, In Progress, In Testing, or Done
+              feature_id: obj.featureId
           }
       };
     }
@@ -401,7 +525,6 @@ module.exports = (robot) => {
     }).catch(err => {
       res.reply(err);
     })
-
 
   });
 };
